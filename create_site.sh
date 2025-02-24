ba1330373d09b4daf8a7d0e768a778bc90d7c839
hasanalasafra <hasanalasafra@gmail.com>
2025-02-24 15:53:50 +0300

    fix for aws

--- create_site.sh
+++ create_site.sh
@@ -32,49 +32,28 @@ else
 
   # Generate VirtualHost configuration
 
-  cat <<EOF > "$confFile"
-
+  sudo bash -c "cat > '$confFile' << 'EOF'
 <VirtualHost *:80>
-
   ServerAdmin $ServerAdminEmail
-
   ServerName $newServer
-
   DocumentRoot /var/www/html/$newServer
-
   DirectoryIndex index.html
 
-# if index.html file dne the remove the line above
-
   <Directory /var/www/html/$newServer>
-
     Options Indexes FollowSymLinks MultiViews
-
     AllowOverride All
-
     Order allow,deny
-
     Allow from all
-
   </Directory>
 
-# remove this if server contains 1 php version
-
-  <FilesMatch \.php$>
-
-    # For Apache version 2.4.10 and above, use SetHandler to run PHP as a fastCGI process server
-
-     SetHandler "proxy:unix:/run/php/php$version-fpm.sock|fcgi://localhost"
-
+  <FilesMatch \.php\$>
+    SetHandler \"proxy:unix:/run/php/php$version-fpm.sock|fcgi://localhost\"
   </FilesMatch>
 
-  ErrorLog ${APACHE_LOG_DIR}/${newServer}_error.log
-
-  CustomLog ${APACHE_LOG_DIR}/${newServer}_access.log combined
-
+  ErrorLog \${APACHE_LOG_DIR}/${newServer}_error.log
+  CustomLog \${APACHE_LOG_DIR}/${newServer}_access.log combined
 </VirtualHost>
-
-EOF
+EOF"
 
   cd /etc/apache2/sites-available
 
